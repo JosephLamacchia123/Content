f4bdb9826a47fc26dd3908d8b0e0aae018bec750
Jason <jasonfbezerra@gmail.com>
2023-06-28 14:23:13 -0400

    made edits

--- GetNewsArticle/LambdaCode.py
+++ GetNewsArticle/LambdaCode.py
@@ -1,9 +1,52 @@
-from newsdataapi import NewsDataApiClient
+import unicodedata
+import requests
+from newspaper import Article
+import json
 
-# API key authorization, Initialize the client with your API key
+def getNewsArticle():
+    site = "https://gnews.io/api/v4/top-headlines?lang=en&country=us"
+    params = {
+        "category": "nation",
+        "max": "1",
+        "apikey": "20b9104ce03229cff48aa78a604b2f0c",
+    }
 
-api = NewsDataApiClient(apikey="pub_207422f778de16c8efc49f6c31ab50b1726ad")
+    response = requests.request("GET", site, params=params)
 
-# You can pass empty or with request parameters {ex. (country = "us")}
+    #function test by printing article titles, can be removed
+    title = response.json().get("articles")[0].get("title")
 
-response = api.news_api( q= "ronaldo" , country = "us")
+    url = response.json().get("articles")[0].get("url")
+
+    article_site = Article(url)
+    article_site.download()
+    article_site.parse()
+
+    article = { "title": title.encode("ascii", "ignore").decode('utf-8'), "text": article_site.text.encode("ascii", "ignore").decode('utf-8').replace('\n','') }
+    print(json.dumps(article))
+    return(json.dumps(article))
+
+getNewsArticle()
+
+"""
+Bing Version
+
+def getNewsArticle():
+    site = "https://api.bing.microsoft.com/v7.0/news/trendingtopics"
+    count = 2
+    headers = {
+        "Ocp-Apim-Subscription-Key": "a97cb4f904c344098c959f46964d844b"
+    }
+
+    response = requests.request("GET", site, headers=headers)
+
+    #function test by printing article titles, can be removed
+    titles = [] 
+    for x in range(count):
+        titles.append(response.json().get("value")[x].get("query"))
+        print(titles[x])
+
+    return response.json()['value']
+
+getNewsArticle()
+"""
